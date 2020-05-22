299447b1cb4421142629d263a02cf6133e3c9ad6
Alistair A. Israel <aisrael@gmail.com>
2020-05-22 20:23:44 +0800

    Added some more functions

--- lib/eda.ex
+++ lib/eda.ex
@@ -14,4 +14,34 @@ defmodule EDA do
     Logger.debug(inspect(res))
     s
   end
+
+  @doc """
+  Just outputs "Hello, world!" if given no parameters.
+  """
+  @spec hello(who :: String.t()) :: String.t()
+  def hello(who \\ "world") do
+    s = "Hello, #{who}!"
+    Logger.debug(s)
+    s
+  end
+
+  @doc """
+  An infinite receive loop
+  """
+  @spec infinite_receive_loop :: no_return
+  def infinite_receive_loop() do
+    receive do
+      {:hello, who} -> hello(who)
+      {:compute, s} -> compute(s)
+      {:hello_reply, pid, who} -> send(pid, hello(who))
+      {:compute_reply, pid, s} -> send(pid, compute(s))
+      x -> Logger.debug(inspect(x))
+    after
+      5000 ->
+        Logger.debug("Timeout after 5 seconds")
+        :ok
+    end
+
+    infinite_receive_loop()
+  end
 end
