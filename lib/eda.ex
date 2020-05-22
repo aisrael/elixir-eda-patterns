948a65754492fb3448043f27f85d6aa4a8699de1
Alistair A. Israel <aisrael@gmail.com>
2020-05-22 20:06:23 +0800

    Add a function that actually does something

--- lib/eda.ex
+++ lib/eda.ex
@@ -3,16 +3,15 @@ defmodule EDA do
   Documentation for `EDA`.
   """
 
+  require Logger
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EDA.hello()
-      :world
-
+  Performs a time-consuming computation (Bcrypt)
   """
-  def hello do
-    :world
+  @spec compute(password :: String.t()) :: String.t()
+  def compute(password \\ "correct horse battery staple") do
+    {_, s} = res = :timer.tc(fn -> Bcrypt.hash_pwd_salt(password) end)
+    Logger.debug(inspect(res))
+    s
   end
 end
